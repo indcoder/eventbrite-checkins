3d10fe6f816d5f250d338f80365c61ea4156f8ba
Augustine Correa <augcor@gmail.com>
2017-03-26 20:55:35 +0530

    And some more linting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const Promise = require('bluebird');
 const callEventBriteAPI = require('../lib/callAPI');
 const host = 'https://www.eventbriteapi.com/v3';
@@ -10,20 +8,22 @@ const debug = require('debug')('eb-checkin');
  * @param {string} accessToken
  * @param {number} eventID
  * @param {string} flag
- * @return {[string]} 
+ * @return {[string]}
  */
-function getCheckedInAttendees (accessToken, eventID, flag) {  
+
+function getCheckedInAttendees(accessToken, eventID, flag) {
   return Promise
           .try(() =>{
-              const path = `/events/${eventID}/attendees/?token=${accessToken}`;
-              debug(`Url is ${host + path}`);
-              if(arguments.length < 2 || (typeof accessToken === undefined) || (eventID === undefined))
-                throw new Error("INCORRECT_ARGUMENTS");
-  
-              if (flag && flag !== 'noshow')
-                throw new Error('INCORRECT_FLAG');
+            const path = `/events/${eventID}/attendees/?token=${accessToken}`;
+            debug(`Url is ${host + path}`);
+            if(arguments.length < 2 || (typeof accessToken === 'undefined') || (eventID === 'undefined')) {
+              throw new Error('INCORRECT_ARGUMENTS');
+            }
+            if (flag && flag !== 'noshow') {
+              throw new Error('INCORRECT_FLAG');
+            }
 
-              return callEventBriteAPI(host + path);
+            return callEventBriteAPI(host + path);
           })
           .then(body => {
             const result = JSON.parse(body);
@@ -31,27 +31,26 @@ function getCheckedInAttendees (accessToken, eventID, flag) {
             return result.attendees;
           })
           .filter(attendee =>{
-            if( flag === 'noshow')
-              return attendee.checked_in === false && attendee.cancelled === false && attendee.refunded === false ;    
-            return attendee.checked_in === true && attendee.cancelled === false && attendee.refunded === false ;
+            if(flag === 'noshow') {
+              return attendee.checked_in === false && attendee.cancelled === false && attendee.refunded === false;
+            }
+            return attendee.checked_in === true && attendee.cancelled === false && attendee.refunded === false;
           })
           .map(attendee => {
             debug(`The filtered attendee name is ${attendee.profile.name}`);
-              return {
-                "name" : attendee.profile.name ,
-                "email" : attendee.profile.email,
-                "evenbrite_id" : attendee.id 
-              };
+            return {
+              'name': attendee.profile.name,
+              'email': attendee.profile.email,
+              'evenbrite_id': attendee.id,
+            };
           })
           .catch(err => {
             console.error('Error ', err);
             throw new Error(err);
           });
 
-};
-
-
-
-module.exports = {  
-  getCheckedInAttendees
 }
+
+module.exports = {
+  getCheckedInAttendees,
+};
